9343ba67508e0e15229edc9da5c4c776f5af75f4
Kyle <kylev006@gmail.com>
2023-04-17 20:49:39 -0700

    sending index and show data

--- server.js
+++ server.js
@@ -11,16 +11,16 @@ app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
 })
 
-// ROUTES
-app.get('/', (req, res) => {
-    res.send('Welcome to an Awesome App about Breads')
-  })
+// Breads
+const breadsController = require('./controllers/breads_controller.js')
+app.use('/breads', breadsController)
   
-  // Breads
-  const breadsController = require('./controllers/breads_controller.js')
-  app.use('/breads', breadsController)
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
   
-  
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
